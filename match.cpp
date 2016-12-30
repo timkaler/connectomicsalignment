3e36336876e37e76b20cf381b6acfc811ce8106e
TB Schardl <neboat@mit.edu>
2016-12-30 15:34:42 +0000

    [match] Bug fixes to establish compatibility with Python rh_aligner pipeline.

--- match.cpp
+++ match.cpp
@@ -22,6 +22,7 @@
 
 // Helper method to check if a key point is inside a given bounding
 // box.
+__attribute__((const))
 static bool bbox_contains(float pt_x, float pt_y,
                           int x_start, int x_finish,
                           int y_start, int y_finish) {
@@ -51,11 +52,6 @@ static void match_features(std::vector< cv::DMatch > &matches,
                    raw_matches,
                    2);
 
-  // TRACE_1("    -- [%d_%d, %d_%d] n_raw_matches: %lu\n",
-  //         a_tile->mfov_id, a_tile->index,
-  //         b_tile->mfov_id, b_tile->index,
-  //         raw_matches.size());
-
   // Apply ratio test
   for (size_t i = 0; i < raw_matches.size(); i++) {
     if (raw_matches[i][0].distance <
@@ -65,143 +61,22 @@ static void match_features(std::vector< cv::DMatch > &matches,
   }
 }
 
-void get_range(
-               cv::Point2f pt,
-               int d1_size,
-               int d2_size,
-               int patch_leg,
-               int *p_d1_start,
-               int *p_d1_finish,
-               int *p_d2_start,
-               int *p_d2_finish) {
-
-  int pt_d2 = pt.x;
-  int pt_d1 = pt.y;
-
-  int d1_start = pt_d1 - patch_leg;
-  int d1_finish = pt_d1 + patch_leg;
-
-  int d2_start = pt_d2 - patch_leg;
-  int d2_finish = pt_d2 + patch_leg;
-
-  if (d1_start < 0) {
-    d1_start = 0;
-  }
-
-  if (d1_finish > d1_size) {
-    d1_finish = d1_size;
-  }
-
-  if (d2_start < 0) {
-    d2_start = 0;
-  }
-
-  if (d2_finish > d2_size) {
-    d2_finish = d2_size;
-  }
-
-  *p_d1_start = d1_start;
-  *p_d1_finish = d1_finish;
-
-  *p_d2_start = d2_start;
-  *p_d2_finish = d2_finish;
-}
-
-float compare_patches(
-                      tile_data_t *p_tile_dst,
-                      tile_data_t *p_tile_src,
-                      cv::Point2f dst_pt,
-                      cv::Point2f src_pt) {
-
-  int rows = p_tile_src->p_image->rows;
-  int cols = p_tile_src->p_image->cols;
-
-  int patch_leg = 30;
-
-  ASSERT(rows == p_tile_dst->p_image->rows);
-  ASSERT(cols == p_tile_dst->p_image->cols);
-
-  int d1_start;
-  int d1_finish;
-  int d2_start;
-  int d2_finish;
-
-  get_range(
-            dst_pt,
-            rows,
-            cols,
-            patch_leg,
-            &d1_start,
-            &d1_finish,
-            &d2_start,
-            &d2_finish);
-
-  int dst_d1_len = d1_finish - d1_start;
-  int dst_d2_len = d2_finish - d2_start;
-
-  cv::Mat patch_dst = (*p_tile_dst->p_image)(cv::Rect(
-                                                      d2_start,
-                                                      d1_start,
-                                                      dst_d2_len,
-                                                      dst_d1_len));
-
-  get_range(
-            src_pt,
-            rows,
-            cols,
-            patch_leg,
-            &d1_start,
-            &d1_finish,
-            &d2_start,
-            &d2_finish);
-
-  int src_d1_len = d1_finish - d1_start;
-  int src_d2_len = d2_finish - d2_start;
-
-  cv::Mat patch_src = (*p_tile_src->p_image)(cv::Rect(
-                                                      d2_start,
-                                                      d1_start,
-                                                      src_d2_len,
-                                                      src_d1_len));
-
-  if ((dst_d1_len != src_d1_len) || (dst_d2_len != src_d2_len)) {
-    return 0;
-  }
-
-  cv::Mat m_res = cv::Mat::zeros(1, 1, CV_32FC1);
-
-  cv::matchTemplate(patch_dst, patch_src, m_res, CV_TM_CCORR_NORMED);
-
-  float f_res = m_res.at<float>(0, 0);
-
-  return f_res;
-}
-
-double get_angle(cv::Point2f p1, cv::Point2f p2) {
-  double angle = atan2(p2.y - p1.y, p2.x - p1.x) * 180.0 / CV_PI;
-
-  if (angle < 0) {
-    angle = angle + 360;
-  }
-
-  //printf("get_angle: %f\n", angle);
-
-  return angle;
-}
-
-static double dist(cv::Point2f a_pt, cv::Point2f b_pt) {
+__attribute__((const))
+static double dist(const cv::Point2f a_pt, const cv::Point2f b_pt) {
   double x_delta = a_pt.x - b_pt.x;
   double y_delta = a_pt.y - b_pt.y;
   return std::sqrt((x_delta * x_delta) + (y_delta * y_delta));
 }
 
-static void save_tile_matches(tile_data_t *a_data, tile_data_t *b_data,
-                              std::vector< cv::Point2f > &match_points_a,
-                              std::vector< cv::Point2f > &match_points_b,
-                              std::vector< cv::Point2f > &match_points_a_fixed,
-                              // std::vector< cv::Point2f > &match_points_b_fixed,
-                              size_t num_matches,
-                              std::string &out_filepath) {
+const int INDENT_SPACES = 4;
+
+static void save_tile_matches(size_t num_matches,
+                              const std::string &out_filepath,
+                              const tile_data_t *a_data,
+                              const tile_data_t *b_data,
+                              const std::vector< cv::Point2f > *match_points_a,
+                              const std::vector< cv::Point2f > *match_points_b,
+                              const std::vector< cv::Point2f > *match_points_a_fixed) {
 
   FILE *fp;
 
@@ -209,7 +84,6 @@ static void save_tile_matches(tile_data_t *a_data, tile_data_t *b_data,
 
   TRACE_1("Writing %s\n", out_filepath.c_str());
 
-  const int INDENT_SPACES = 4;
   fp = fopen(out_filepath.c_str(), "wb");
   // Output prologue
   fprintf(fp, "[\n");
@@ -217,84 +91,89 @@ static void save_tile_matches(tile_data_t *a_data, tile_data_t *b_data,
 
   // Output correspondence point pairs
   TRACE_1("  -- outputting %lu matches\n", num_matches);
-  fprintf(fp, "%*s %s\n", 2*INDENT_SPACES, "",
+  fprintf(fp, "%*s%s\n", 2*INDENT_SPACES, "",
           "\"correspondencePointPairs\": [");
   for (size_t i = 0; i < num_matches; ++i) {
-    fprintf(fp, "%*s %s\n", 3*INDENT_SPACES, "", "{");
+    fprintf(fp, "%*s%s\n", 3*INDENT_SPACES, "", "{");
 
     // Emit distance
-    fprintf(fp, "%*s %s %f,\n", 4*INDENT_SPACES, "",
+    fprintf(fp, "%*s%s %f, \n", 4*INDENT_SPACES, "",
             "\"dist_after_ransac\":",
-            dist(match_points_a_fixed[i], match_points_b[i]));
+            dist((*match_points_a_fixed)[i], (*match_points_b)[i]));
     // Emit first point
-    fprintf(fp, "%*s %s\n", 4*INDENT_SPACES, "",
+    fprintf(fp, "%*s%s\n", 4*INDENT_SPACES, "",
             "\"p1\": {");
 
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
+    fprintf(fp, "%*s%s\n", 5*INDENT_SPACES, "",
             "\"l\": [");
-    fprintf(fp, "%*s %f,\n", 6*INDENT_SPACES, "",
-            match_points_a[i].x);
-    fprintf(fp, "%*s %f\n", 6*INDENT_SPACES, "",
-            match_points_a[i].y);
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
-            "],");
-
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
-            "\"w\": [");
-    fprintf(fp, "%*s %f,\n", 6*INDENT_SPACES, "",
-            match_points_a[i].x + a_data->x_start);
-    fprintf(fp, "%*s %f\n", 6*INDENT_SPACES, "",
-            match_points_a[i].y + a_data->y_start);
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
-            "],");
-
-    fprintf(fp, "%*s %s\n", 4*INDENT_SPACES, "",
-            "},");
-
-    // Emit second point
-    fprintf(fp, "%*s %s\n", 4*INDENT_SPACES, "",
-            "\"p2\": {");
-
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
-            "\"l\": [");
-    fprintf(fp, "%*s %f,\n", 6*INDENT_SPACES, "",
-            match_points_b[i].x);
-    fprintf(fp, "%*s %f\n", 6*INDENT_SPACES, "",
-            match_points_b[i].y);
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
-            "],");
-
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
-            "\"w\": [");
-    fprintf(fp, "%*s %f,\n", 6*INDENT_SPACES, "",
-            match_points_b[i].x + b_data->x_start);
-    fprintf(fp, "%*s %f\n", 6*INDENT_SPACES, "",
-            match_points_b[i].y + b_data->y_start);
-    fprintf(fp, "%*s %s\n", 5*INDENT_SPACES, "",
+    fprintf(fp, "%*s%f, \n", 6*INDENT_SPACES, "",
+            (*match_points_a)[i].x);
+    fprintf(fp, "%*s%f\n", 6*INDENT_SPACES, "",
+            (*match_points_a)[i].y);
+    fprintf(fp, "%*s%s, \n", 5*INDENT_SPACES, "",
             "]");
 
-    fprintf(fp, "%*s %s\n", 4*INDENT_SPACES, "",
+    fprintf(fp, "%*s%s\n", 5*INDENT_SPACES, "",
+            "\"w\": [");
+    fprintf(fp, "%*s%f, \n", 6*INDENT_SPACES, "",
+            (*match_points_a)[i].x + a_data->x_start);
+    fprintf(fp, "%*s%f\n", 6*INDENT_SPACES, "",
+            (*match_points_a)[i].y + a_data->y_start);
+    fprintf(fp, "%*s%s\n", 5*INDENT_SPACES, "",
+            "]");
+
+    fprintf(fp, "%*s%s, \n", 4*INDENT_SPACES, "",
             "}");
 
-    fprintf(fp, "%*s %s\n", 3*INDENT_SPACES, "", "},");
+    // Emit second point
+    fprintf(fp, "%*s%s\n", 4*INDENT_SPACES, "",
+            "\"p2\": {");
+
+    fprintf(fp, "%*s%s\n", 5*INDENT_SPACES, "",
+            "\"l\": [");
+    fprintf(fp, "%*s%f, \n", 6*INDENT_SPACES, "",
+            (*match_points_b)[i].x);
+    fprintf(fp, "%*s%f\n", 6*INDENT_SPACES, "",
+            (*match_points_b)[i].y);
+    fprintf(fp, "%*s%s, \n", 5*INDENT_SPACES, "",
+            "]");
+
+    fprintf(fp, "%*s%s\n", 5*INDENT_SPACES, "",
+            "\"w\": [");
+    fprintf(fp, "%*s%f, \n", 6*INDENT_SPACES, "",
+            (*match_points_b)[i].x + b_data->x_start);
+    fprintf(fp, "%*s%f\n", 6*INDENT_SPACES, "",
+            (*match_points_b)[i].y + b_data->y_start);
+    fprintf(fp, "%*s%s\n", 5*INDENT_SPACES, "",
+            "]");
+
+    fprintf(fp, "%*s%s\n", 4*INDENT_SPACES, "",
+            "}");
+
+    fprintf(fp, "%*s%s", 3*INDENT_SPACES, "", "}");
+    if (i+1 < num_matches)
+      fprintf(fp, ", ");
+    fprintf(fp, "\n");
   }
-  fprintf(fp, "%*s %s\n", 2*INDENT_SPACES, "",
-          "],");
+  fprintf(fp, "%*s%s, \n", 2*INDENT_SPACES, "",
+          "]");
 
   // Output mipmapLevel
-  fprintf(fp, "%*s %s: %d,\n", 2*INDENT_SPACES, "",
+  fprintf(fp, "%*s%s: %d, \n", 2*INDENT_SPACES, "",
           "\"mipmapLevel\"", 0);
 
   // TODO: Output model
-
+  //
+  // TB: This field does not seem to be used by the optimization step.
+  
   // Output input file images
-  fprintf(fp, "%*s %s: \"file://%s\",\n", 2*INDENT_SPACES, "",
+  fprintf(fp, "%*s%s: \"file://%s\", \n", 2*INDENT_SPACES, "",
           "\"url1\"", a_data->filepath);
-  fprintf(fp, "%*s %s: \"file://%s\"\n", 2*INDENT_SPACES, "",
+  fprintf(fp, "%*s%s: \"file://%s\"\n", 2*INDENT_SPACES, "",
           "\"url2\"", b_data->filepath);
   
   // Output epilogue
-  fprintf(fp, "%*s %s\n", INDENT_SPACES, "", "}");
+  fprintf(fp, "%*s%s\n", INDENT_SPACES, "", "}");
   fprintf(fp, "]");
 
   fclose(fp);
@@ -308,18 +187,34 @@ static void save_tile_matches(tile_data_t *a_data, tile_data_t *b_data,
 void compute_tile_matches(align_data_t *p_align_data) {
 
   TRACE_1("compute_tile_matches: start\n");
-
-  // std::vector<cv::Point2f> sec_pts_dst[MAX_SECTIONS][MAX_TILES];
-  // std::vector<cv::Point2f> sec_pts_src[MAX_SECTIONS][MAX_TILES];
+  std::string out_filepath_base =
+    std::string(p_align_data->output_dirpath) + "/matched_sifts/W01_Sec";
 
   // Iterate over all pairs of tiles
   for (int sec_id = 0; sec_id < p_align_data->n_sections; ++sec_id) {
     section_data_t *p_sec_data = &(p_align_data->sec_data[sec_id]);
-    for (int atile_id = 0; atile_id < p_sec_data->n_tiles; ++atile_id) {
-      for (int btile_id = atile_id + 1; btile_id < p_sec_data->n_tiles; ++btile_id) {
-        if (atile_id == btile_id) continue;
+    // We extract the section id from the first tile.
+    bool got_section_id = false;
+    std::string real_section_id;
+    std::vector< std::string > output_files;
 
-        tile_data_t *a_tile = &(p_sec_data->tiles[atile_id]);
+    for (int atile_id = 0; atile_id < p_sec_data->n_tiles; ++atile_id) {
+      tile_data_t *a_tile = &(p_sec_data->tiles[atile_id]);
+
+      // Compute starts of output file path and output file name.
+      const std::string a_filepath = std::string(a_tile->filepath);
+      const std::string a_timagename = a_filepath.substr(a_filepath.find_last_of("/")+1);
+      if (!got_section_id) {
+        real_section_id = a_timagename.substr(0, a_timagename.find("_"));
+        got_section_id = true;
+      }
+      const std::string out_filepath_start = out_filepath_base + real_section_id + "/";
+      const std::string out_filename_start = std::string("W01_Sec") +
+        real_section_id + std::string("_sift_matches_") +
+        a_timagename.substr(0, a_timagename.find_last_of(".")) + "_"; 
+
+      for (int btile_id = atile_id + 1; btile_id < p_sec_data->n_tiles; ++btile_id) {
+        // if (atile_id == btile_id) continue;
         tile_data_t *b_tile = &(p_sec_data->tiles[btile_id]);
 
         // Skip tiles that don't overlap
@@ -339,39 +234,42 @@ void compute_tile_matches(align_data_t *p_align_data) {
                 b_tile->mfov_id, b_tile->index,
                 b_tile->p_kps->size());
 
-        std::string a_filepath = std::string(a_tile->filepath);
-        std::string a_timagename = a_filepath.substr(a_filepath.find_last_of("/")+1);
-        std::string a_real_section_id = a_timagename.substr(0, a_timagename.find("_"));
-        std::string b_filepath = std::string(b_tile->filepath);
-        std::string b_timagename = b_filepath.substr(b_filepath.find_last_of("/")+1);
-        // std::string b_real_section_id = b_timagename.substr(0, b_timagename.find("_"));
-        std::string out_filepath = std::string(p_align_data->output_dirpath) +
-          "/matched_sifts/W01_Sec" + a_real_section_id + "/";
+        // Compute output file path and output file name.
+        const std::string b_filepath = std::string(b_tile->filepath);
+        const std::string b_timagename = b_filepath.substr(b_filepath.find_last_of("/")+1);
+        std::string out_filepath = out_filepath_start;
         if (a_tile->mfov_id == b_tile->mfov_id) {
           // Intra mfov job
-          out_filepath = out_filepath + "intra/" + std::to_string(a_tile->mfov_id);
+          out_filepath = out_filepath + "intra/" + std::to_string(a_tile->mfov_id) + "/";
         } else {
           // Inter mfov job
-          out_filepath = out_filepath + "inter";
+          out_filepath = out_filepath + "inter/";
         }
         system((std::string("mkdir -p ") + out_filepath).c_str());
-        out_filepath = out_filepath + std::string("/W01_Sec") +
-          a_real_section_id + std::string("_sift_matches_") +
-          a_timagename.substr(0, a_timagename.find_last_of(".")) +
+        const std::string out_filename = out_filename_start +
           b_timagename.substr(0, b_timagename.find_last_of(".")) +
           std::string(".json");
+        out_filepath = out_filepath + out_filename;
+
+        output_files.push_back("file://" + out_filepath);
 
         // Check that both tiles have enough features to match.
         if (a_tile->p_kps->size() < MIN_FEATURES_NUM) {
           TRACE_1("Less than %d features in tile %d_%d, saving empty match file\n",
                   MIN_FEATURES_NUM,
                   a_tile->mfov_id, a_tile->index);
+          save_tile_matches(0, out_filepath,
+                            a_tile, b_tile,
+                            nullptr, nullptr, nullptr);
           continue;
         }
         if (b_tile->p_kps->size() < MIN_FEATURES_NUM) {
           TRACE_1("Less than %d features in tile %d_%d, saving empty match file\n",
                   MIN_FEATURES_NUM,
                   b_tile->mfov_id, b_tile->index);
+          save_tile_matches(0, out_filepath,
+                            a_tile, b_tile,
+                            nullptr, nullptr, nullptr);
           continue;
         }
 
@@ -445,12 +343,18 @@ void compute_tile_matches(align_data_t *p_align_data) {
           TRACE_1("Less than %d features in the overlap in tile %d_%d, saving empty match file\n",
                   MIN_FEATURES_NUM,
                   a_tile->mfov_id, a_tile->index);
+          save_tile_matches(0, out_filepath,
+                            a_tile, b_tile,
+                            nullptr, nullptr, nullptr);
           continue;
         }
         if (btile_kps_in_overlap.size() < MIN_FEATURES_NUM) {
           TRACE_1("Less than %d features in the overlap in tile %d_%d, saving empty match file\n",
                   MIN_FEATURES_NUM,
                   b_tile->mfov_id, b_tile->index);
+          save_tile_matches(0, out_filepath,
+                            a_tile, b_tile,
+                            nullptr, nullptr, nullptr);
           continue;
         }
 
@@ -504,6 +408,9 @@ void compute_tile_matches(align_data_t *p_align_data) {
         if (filtered_matches.size() < MIN_FEATURES_NUM) {
           TRACE_1("Less than %d matched features, saving empty match file\n",
                   MIN_FEATURES_NUM);
+          save_tile_matches(0, out_filepath,
+                            a_tile, b_tile,
+                            nullptr, nullptr, nullptr);
           continue;
         }
 
@@ -517,35 +424,44 @@ void compute_tile_matches(align_data_t *p_align_data) {
           }
 
           cv::Mat model = cv::estimateRigidTransform(match_points_a, match_points_b, false);
-          TRACE_1("    -- [%d_%d, %d_%d] rigid transform matrix: %d by %d\n",
+          TRACE_1("    -- [%d_%d, %d_%d] estimated a %d by %d affine transform matrix.\n",
                   a_tile->mfov_id, a_tile->index,
                   b_tile->mfov_id, b_tile->index,
                   model.rows, model.cols);
           if (0 == model.rows) {
-            TRACE_1("Could not estimate rigid transform, saving empty match file\n");
+            TRACE_1("Could not estimate affine transform, saving empty match file\n");
+            save_tile_matches(0, out_filepath,
+                              a_tile, b_tile,
+                              nullptr, nullptr, nullptr);
             continue;
           }
 
-          // Extract the match points
-          // std::vector< cv::Point2f > match_points_a, match_points_b;
-          // for (size_t i = 0; i < filtered_matches.size(); ++i) {
-          //   match_points_a.push_back(atile_kps_in_overlap[filtered_matches[i].queryIdx].pt);
-          //   match_points_b.push_back(btile_kps_in_overlap[filtered_matches[i].trainIdx].pt);
-          // }
           std::vector< cv::Point2f > match_points_a_fixed;
           // std::vector< cv::Point2f > match_points_b_fixed;
           cv::transform(match_points_a, match_points_a_fixed, model);
           // cv::transform(match_points_b, match_points_b_fixed, model);
 
-          save_tile_matches(a_tile, b_tile,
-                            match_points_a, match_points_b,
-                            match_points_a_fixed,
-                            filtered_matches.size(), out_filepath);
+          save_tile_matches(filtered_matches.size(), out_filepath,
+                            a_tile, b_tile,
+                            &match_points_a, &match_points_b,
+                            &match_points_a_fixed);
         }
       }  // for (btile_id)
     }  // for (atile_id)
+
+    if (got_section_id) {
+      const std::string matched_sifts_files =
+        std::string(p_align_data->output_dirpath) +
+        "/W01_Sec" + real_section_id + "_matched_sifts_files.txt";
+      TRACE_1("Recording matched sifts files in %s\n",
+              matched_sifts_files.c_str());
+
+      FILE *fp = fopen(matched_sifts_files.c_str(), "wb");
+      for (size_t i = 0; i < output_files.size(); ++i)
+        fprintf(fp, "%s\n", output_files[i].c_str());
+      fclose(fp);
+    }
   }  // for (sec_id)
 
   TRACE_1("compute_tile_matches: finish\n");
-
 }
