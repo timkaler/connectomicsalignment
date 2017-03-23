d394cf50b30ee05eb7b2c63abee4ab49a03cd003
Tim Kaler <tfk@mit.edu>
2017-03-23 21:50:50 +0000

    add missing files

---
+++ gen_result.sh
@@ -0,0 +1,6 @@
+/efs/home/tfk/rh_aligner/run_render.sh -s 0.3 --from_x 0 --to_x 40000 --from_y 0 --to_y 40000 /efs/home/tfk/alignment_newrepo/temp/W01_Sec001_montaged.json section1
+#./run_render.sh -s 0.3 --from_x 0 --to_x 40000 --from_y 0 --to_y 40000 /efs/home/tfk/alignment_newrepo/temp/W01_Sec002_montaged.json section2
+#./run_render.sh -s 0.3 --from_x 0 --to_x 40000 --from_y 0 --to_y 40000 /efs/home/tfk/alignment_newrepo/temp/W01_Sec003_montaged.json section3
+#./run_render.sh -s 0.3 --from_x 0 --to_x 40000 --from_y 0 --to_y 40000 /efs/home/tfk/alignment_newrepo/temp/W01_Sec004_montaged.json section4
+#./run_render.sh -s 0.3 --from_x 10000 --to_x 30000 --from_y 10000 --to_y 30000 /efs/home/tfk/alignment_newrepo/temp/W01_Sec011_montaged.json section1
+#./run_render.sh -s 0.3 --from_x 10000 --to_x 30000 --from_y 10000 --to_y 30000 /efs/home/tfk/alignment_newrepo/temp/W01_Sec012_montaged.json section2
---
+++ run_big_test_two.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+
+
+export CILK_NWORKERS=16
+NUMANODE=0
+SECTION=0
+WORKINGDIR=$PWD
+OUTPUTDIR=/efs/home/tfk/temp/
+
+#tee output_file_tfk.out &
+#./setup.sh gdb --args ./run_align 1 \
+#./setup.sh numactl --cpunodebind=$NUMANODE ./run_align 1 \
+#    $SECTION 2 \
+#    $PWD/data/3dtest1.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR
+
+./setup.sh numactl --cpunodebind=$NUMANODE ./run_align 1 \
+    $SECTION 1 \
+    $PWD/testing3d.txt \
+    $OUTPUTDIR \
+    $OUTPUTDIR
+
+#./setup.sh ./run_align 1 \
+#    24 1 \
+#    $PWD/data/txtspecs_iarpa.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR
+
+
+#./setup.sh ./run_align 1 \
+#    24 1 \
+#    $PWD/data/txtspecs_iarpa.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR
+
+
+#./setup.sh ./run_align 1 \
+#    25 1 \
+#    $PWD/data/txtspecs_iarpa.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR
+
+
+#./setup.sh ./run_align 1 \
+#    10 2 \
+#    $PWD/data/txtspecs.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR 
+#&& ./setup.sh ./run_align 1 \
+#    11 1 \
+#    $PWD/data/txtspecs.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR && ./setup.sh ./run_align 1 \
+#    12 1 \
+#    $PWD/data/txtspecs.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR && ./setup.sh ./run_align 1 \
+#    13 1 \
+#    $PWD/data/txtspecs.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR
+
+
+
+
+
+
+
+
+
+#
+#    ./run_align 1 \
+#    13 1 \
+#    $PWD/txtspecs.txt \
+#    $OUTPUTDIR \
+#    $OUTPUTDIR
