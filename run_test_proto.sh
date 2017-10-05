b360835d0aa86b200b8a28451df17c760be28cf8
Tim Kaler <tfk@mit.edu>
2017-10-05 17:49:50 +0000

    update some paths in setup.sh

--- run_test_proto.sh
+++ run_test_proto.sh
@@ -3,7 +3,9 @@
 
 
 export CILK_NWORKERS=18
-export LD_LIBRARY_PATH=/efs/home/wheatman/install_dir/protobufs/lib:$LD_LIBRARY_PATH
+# no longer needed since its added to setup.sh
+#export LD_LIBRARY_PATH=/efs/home/wheatman/install_dir/protobufs/lib:$LD_LIBRARY_PATH
+
 NUMANODE=0
 SECTION=0
 WORKINGDIR=$PWD
