a17c9b727c90e79dfd8594fcb6c349c0866398ce
Tim Kaler <tfk@mit.edu>
2018-05-03 00:52:36 +0000

    add missing files

---
+++ tilesifttask.cpp
@@ -0,0 +1,44 @@
+#include "tilesifttask.hpp"
+namespace tfk {
+
+    TileSiftTask::~TileSiftTask () {
+    }
+
+    TileSiftTask::TileSiftTask (ParamDB* paramDB, Tile* tile) {
+      this->paramDB = paramDB;
+      this->tile = tile;
+      this->task_type_id = 1;
+    }
+
+    void TileSiftTask::compute_with_params(MRParams* mr_params_local) {
+
+      MRParams* mr_params = mr_params_local;
+
+      tfk::params new_params;
+      new_params.scale_x = mr_params->get_float_param("scale");
+      new_params.scale_y = mr_params->get_float_param("scale");
+
+      //printf("scale x %f scale y %f\n", new_params.scale_x, new_params.scale_y);
+      new_params.num_features = mr_params->get_int_param("num_features");
+      new_params.num_octaves = mr_params->get_int_param("num_octaves");
+      new_params.contrast_threshold = 0.015;//mr_params->get_float_param("contrast_threshold");
+      new_params.edge_threshold = 6;// mr_params->get_float_param("edge_threshold");
+      new_params.sigma = 1.2;//mr_params->get_float_param("sigma");
+
+      tile->compute_sift_keypoints2d_params(new_params, tile_keypoints,
+                                              tile_desc, tile);
+    }
+
+    void TileSiftTask::commit() {
+      // do nothing
+    }
+
+    bool TileSiftTask::error_check(float prob) {
+      return true;
+    }
+
+    void TileSiftTask::get_parameter_options(std::vector<tfk::MRParams*>* vec) {
+      //TODO(wheatman) figure out what the parameters of this function are
+    }
+}
+
---
+++ tilesifttask.hpp
@@ -0,0 +1,26 @@
+
+#include "mrtask.hpp"
+#include "stack.hpp"
+
+#ifndef TILESIFTTASK
+#define TILESIFTTASK
+
+namespace tfk {
+
+class TileSiftTask : public MRTask {
+  public:
+    Tile* tile;
+    std::vector<cv::KeyPoint> tile_keypoints;
+    cv::Mat tile_desc;
+
+    TileSiftTask (ParamDB* paramDB, Tile* tile);
+    ~TileSiftTask ();
+    void compute_with_params(tfk::MRParams* mr_params_local);
+    void commit();
+    bool error_check(float false_negative_rate);
+    void get_parameter_options(std::vector<tfk::MRParams*>* vec);
+};
+
+} // end namespace tfk
+#endif
+
